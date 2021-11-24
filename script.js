8727280ae50fab3f33dd59116d2ee0fc7c52ba17
Muzaffar-Xadjayev <muzaffar010125@gmail.com>
2021-11-24 21:39:05 +0500

    new commit

---
+++ script.js
@@ -0,0 +1,47 @@
+let txt1 = document.getElementById('txt')
+
+
+
+
+function myFunc1() {
+    txt1.style.fontFamily = 'Times New Roman'
+    txt1.style.fontStyle = 'italic'
+    txt1.style.color = 'black'
+    document.body.style.backgroundColor = 'red'
+}
+function myFunc2() {
+    txt1.style.fontFamily = 'Arial'
+    txt1.style.fontStyle = 'normal'
+    txt1.style.color = 'black'
+    document.body.style.backgroundColor = 'blue'
+}
+function myFunc3() {
+    txt1.style.fontFamily = 'monospace'
+    txt1.style.fontStyle = 'inherit'
+    txt1.style.color = 'black'
+    document.body.style.backgroundColor = 'Yellow'
+}
+function myFunc4() {
+    txt1.style.fontFamily = 'cursive'
+    txt1.style.fontStyle = 'italic'
+    txt1.style.color = 'black'
+    document.body.style.backgroundColor = 'grey'
+}
+function myFunc5 () {
+    txt1.style.fontFamily = 'sans'
+    txt1.style.fontStyle = 'normal'
+    txt1.style.color = 'black'
+    document.body.style.backgroundColor = 'green'
+}
+function myFunc6 () {
+    txt1.style.fontFamily = 'sans-serif'
+    txt1.style.fontStyle = 'italic'
+    txt1.style.color = 'white'
+    document.body.style.backgroundColor = 'black'
+}
+function myFunc7() {
+    txt1.style.fontFamily = 'arial'
+    txt1.style.fontStyle = 'normal'
+    txt1.style.color = 'black'
+    document.body.style.backgroundColor = 'white'
+}
